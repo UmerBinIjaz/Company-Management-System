d9ebdf720cb6794b5938ca786555c406c2484b5e
UmerBinIjaz <uk302269@gmail.com>
2025-05-18 01:27:43 +0500

    Update teachers.php

--- teachers.php
+++ teachers.php
@@ -501,7 +501,7 @@
     <script src="js/main.js"></script>
     
     <!--====== Map js ======-->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDC3Ip9iVC0nIxC6V14CKLQ1HZNF_65qEQ"></script>
+    <script src="#"></script>
     <script src="js/map-script.js"></script>
 
 </body>
